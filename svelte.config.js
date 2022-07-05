d2457bbd2147ae1a61118f6da7e3e76d6041cfcf
Rob Leroy <robleroy@msn.com>
2022-07-05 09:52:25 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
@@ -12,9 +14,9 @@ const config = {
 		prerender: {
 			default: true
 		},
-    // paths: {
-    //   base: process.env.NODE_ENV === 'development' ? '' : '/atalaya'
-    // }
+    paths: {
+			base: dev ? '' : '/ghtest',
+		},
   },
 
   preprocess: [
